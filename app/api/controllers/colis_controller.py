997377e3652b87b7caab11e8310c46a882c09b24
zakariaelkahri <zakariakahri2003@gmail.com>
2026-01-08 09:17:02 +0100

    the endpoint assign colis added will

--- app/api/controllers/colis_controller.py
+++ app/api/controllers/colis_controller.py
@@ -3,6 +3,14 @@ from sqlalchemy.orm import Session
 from app.models.colis import Colis 
 from app.schemas.colis_schema import ColisCreate
 
+# Module Colis (Le plus gros morceau) :
+# Controller colis_controller.py :
+# Fonction creer_colis : Création simple.
+# Fonction assigner_livreur : Lier un colis à un livreur.
+# Fonction mettre_a_jour_statut : Trés important -> Cette fonction doit aussi créer automatiquement une ligne dans la table HistoriqueStatut à chaque changement.
+# Filtres avancés : Pouvoir lister les colis par Statut ou par Zone.
+
+
 
 def create_colis_logic(db:Session, colis_in:ColisCreate):
     db_colis = Colis(
@@ -21,3 +29,18 @@ def create_colis_logic(db:Session, colis_in:ColisCreate):
     db.refresh(db_colis)
     
     return db_colis 
+
+
+def assign_colis(db:Session, colis_id:int, livreur_id:int):
+    
+    colis = db.query(Colis).filter(Colis.id == colis_id).first()
+    
+    if not colis:
+        return False
+    
+    colis.id_livreur = livreur_id 
+    colis.statut = "en_cours_de_livraison"
+    db.commit()
+    db.refresh(colis)
+    
+    return colis
